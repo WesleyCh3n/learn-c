f4c03e91a9fc86dfbb30b6379f30f5b2b8bfdf29
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-03-10 18:06:28 +0800

    🍁 try to implement multiple pipe

--- pip_exp/pip_process.c
+++ pip_exp/pip_process.c
@@ -3,40 +3,53 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+/* typedef struct process_t {
+ *   pid_t pid;
+ * }process_t;
+ *
+ * process_t subprocess(char *cmd) {
+ *   int fd[2];
+ *   pipe(fd);
+ *   pid_t pid = fork();
+ *   return (process_t){pid};
+ * } */
+
 int main() {
   int fd[2];
   pipe(fd);
+
   pid_t child_pid = fork();
   if (child_pid == 0) {         // first child process
-    close(fd[0]);               // close fd read
-
+    /* close(fd[0]);               // close fd read */
+                                //
     dup2(fd[1], STDOUT_FILENO); // pipe stdout to fd write
     close(fd[1]);               // close fd write
 
-    char *args[] = {"/bin/sh", "-c", "echo \"Hello from \nwesley overflow\"",
-                    NULL};
+    char *args[] = {"/bin/sh", "-c",
+                    "echo \"Hello from \nwesley overflow\nHe again\"", NULL};
     execvp(args[0], args);
-  } else {
-    child_pid = fork();
   }
-  if (child_pid == 0) {
-    close(fd[1]); // close fd write
 
+  child_pid = fork();
+  if (child_pid == 0) {
     dup2(fd[0], STDIN_FILENO); // pipe stdin to fd read
-    close(fd[0]); // close fd read
+    /* close(fd[0]);              // close fd read */
+
+    dup2(fd[1], STDOUT_FILENO); // pipe stdout to fd write
+    close(fd[1]);               // close fd write
 
     char *args[] = {"/bin/sh", "-c", "grep He", NULL};
     execvp(args[0], args);
   }
 
-  /* int status; */
-  /* waitpid(child_pid, &status, WUNTRACED); */
-  printf("Test\n");
   close(fd[1]);
-  /* char buf[15]; */
-  /* read(fd[0], buf, sizeof(buf)); // this will wait for buf full */
-  /* printf("Read from pipe bridge process: %s\n", buf); */
-  close(fd[0]);
+  dup2(fd[0], STDIN_FILENO); // pipe stdin to fd read
+  close(fd[0]);              // close fd read
+
+
+  char *args[] = {"/bin/sh", "-c", "grep again", NULL};
+  execvp(args[0], args);
+  /* close(fd[0]); */
 
   return 0;
 }
